0d663b3f0aba98a3697df96458e6b6e43170e5de
poliferreyra <paoferreyra.fl@gmail.com>
2023-01-26 10:58:54 -0300

    Add open jobs modal

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const $ = (selector) => document.querySelector(selector);
 //**************************** DOM *********************************
 const $createJobNav = $(".create-job-nav")
+console.log($createJobNav)
 const $formCreateJobs = $("#create-job");
-const $jobsFormModal = $(".modal")
+const $createJob = $(".jobsRegister")
+console.log($createJob)
 const $modalClose = $(".modal-close");
 
 const BASE_URL = "https://63d14061120b32bbe8f47c04.mockapi.io/api";
@@ -15,7 +17,9 @@ const newJob = {
   seniority: "Senior",
 };
 // ************************** functions ****************************
-
+const openJobsModal = () =>{
+  $createJob.classList.add("is-active");
+}
 
 // ************************* events **********************************
 // $formCreateJobs.addEventListener("submit", (e) => {
@@ -23,6 +27,8 @@ const newJob = {
 //   registerJobs();
 // });
 
+$createJobNav.addEventListener("click", openJobsModal);
+
 $modalClose.addEventListener("click", () => {
-  $jobsFormModal.classList.remove("is-active");
+  $createJob.classList.remove("is-active");
 });
